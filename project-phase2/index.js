ac519ef94dd5633d0e13529b3649213eef0b2b9f
mjgreene2010 <42053084+mjgreene2010@users.noreply.github.com>
2022-02-18 20:05:38 -0600

    Add files via upload

---
+++ project-phase2/index.js
@@ -0,0 +1,54 @@
+"use strict";
+
+const filterSpan = document.querySelector(".filter");
+const bookFilter = document.getElementById("book-filter");
+const heading = document.querySelector("#book-list-heading");
+const filterBtn = document.querySelector(".submit-filter");
+
+bookFilter.addEventListener("click", function () {
+  let input = document.createElement("input");
+  let btn = document.createElement("button");
+  let span = document.createElement("span");
+
+  if (filterSpan.getAttribute("value") === "false") {
+    input.classList = "book-filter-input";
+    input.setAttribute("type", "text");
+    btn.innerHTML = "Enter";
+    btn.classList = "submit-filter";
+    btn.setAttribute("type", "submit");
+    span.appendChild(input);
+    span.appendChild(btn);
+    span.classList = "input-button";
+    filterSpan.appendChild(span);
+    filterSpan.setAttribute("value", "true");
+  } else if (filterSpan.getAttribute("value") === "true") {
+    filterSpan.lastChild.remove();
+    filterSpan.setAttribute("value", "false");
+  }
+});
+
+const searchSpan = document.querySelector(".search");
+const bookSearch = document.getElementById("book-search");
+const searchBtn = document.querySelector(".submit-search");
+
+bookSearch.addEventListener("click", function () {
+  let input = document.createElement("input");
+  let btn = document.createElement("button");
+  let span = document.createElement("span");
+
+  if (searchSpan.getAttribute("value") === "false") {
+    input.classList = "book-filter-input";
+    input.setAttribute("type", "text");
+    btn.innerHTML = "Enter";
+    btn.classList = "submit-filter";
+    btn.setAttribute("type", "submit");
+    span.appendChild(input);
+    span.appendChild(btn);
+    span.classList = "input-button";
+    searchSpan.appendChild(span);
+    searchSpan.setAttribute("value", "true");
+  } else if (searchSpan.getAttribute("value") === "true") {
+    searchSpan.lastChild.remove();
+    searchSpan.setAttribute("value", "false");
+  }
+});
